69aa90ad820423fdf37c81ac413b2d15487e2265
Ganglet <angshumanchakravertty2@gmail.com>
2024-11-09 22:12:38 +0530

    commit for Web Scrapper

---
+++ Web_scrapper.py
@@ -0,0 +1,7 @@
+urls=[
+    'https://developers.google.com/'
+    'https://developers.google.com/products'
+    'https://developers.google.com/products/developer-platforms-and-os?category=devsitemarketingplatformsandoperatingsystems'
+    'https://cloud.google.com/docs?hl=en'
+    'https://cloud.google.com/docs/ai-ml?hl=en'
+]
